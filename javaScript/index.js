bf95d11d2f2219ce0819d05eb021c9d2362ba5b4
AldanaEscobarNandez <aldana.escobarnandez@gmail.com>
2023-11-02 18:27:35 -0300

    add bootstrap

---
+++ javaScript/index.js
@@ -0,0 +1,20 @@
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
+
+
+//Algortimo con un condicional
+let checkInDate = document.getElementById("checkInDate").value;
+let checkOutDate = document.getElementById("checkOutDate").value;
+let people = document.getElementById("people").value;
+let place = document.getElementById("place").value;
+
+const errorMessage = "Lo sentimos, las fechas ingresadas son incorrectas. Por favor vuelve a intentarlo."
+
+if (checkInDate <= checkOutDate) {
+    errorMessage
+}
+
+
+//Algortimo uticilzando un ciclo
+
+
+//imulador interactivo, la estructura final de tu proyecto integrador
